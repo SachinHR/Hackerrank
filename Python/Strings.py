39ce4011ee6576009b8c34ef327dce8df31c4590
Sachin H R <sachinhr.ran@gmail.com>
2020-04-08 22:06:48 +0530

    Add three string solution

---
+++ Python/Strings.py
@@ -0,0 +1,27 @@
+# Problem 1
+
+#Solution 1
+a = "SaChIn"
+print(a.swapcase())
+_______________________________________________________________________________________________________________________________
+# Problem 2
+
+#Solution 1
+a = "A B C"
+a = a.split()
+print("-".join(a))
+
+#Solution 2
+a = "A B C"
+print("-".join(a.split()))
+
+#Solution 3
+a = "A B C"
+print(a.replace(" ","-"))
+_______________________________________________________________________________________________________________________________
+# Problem 3
+
+#Solution 1
+a = "Sachin"
+b = "H R"
+print("Hello "+ a + " " + b + "! You just delved into python.")
