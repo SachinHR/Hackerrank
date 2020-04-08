db3de968b1ebdbd73dbaf23ca698996875257eae
Sachin H R <sachinhr.ran@gmail.com>
2020-04-08 21:13:23 +0530

    Update Introduction file

--- Python/Introduction.py
+++ Python/Introduction.py
@@ -1,9 +1,9 @@
-#Problem 1
+# Problem 1
 
 print("Hello, World!)
-
-#Problem 2
-
+_____________________________________________________________________________________________________________________________
+# Problem 2
+      
 #Solution 1
 N = int(input())
 if (N % 2 != 0):
@@ -20,23 +20,22 @@ else:
 N = int(input())
 check = { True : "Not Weird", False : "Weird" }
 print(check[ N%2 == 0 and ((N in range(2,5)) or (N > 20)) ])
-
-#Problem 3
-
+_____________________________________________________________________________________________________________________________
+# Problem 3
+      
 #Solution 1
 a = int(input())
 b = int(input())
 print(a + b)
 print(a - b)
 print(a * b)
-
 #Solution 2
 a = int(input())
 b = int(input())
 print("{0} \n{1} \n{2}".format(a+b, a-b, a*b))
-
-#Problem 4
-
+_____________________________________________________________________________________________________________________________
+# Problem 4
+      
 #Solution 1
 n = int(input())
 for i in range(n):
@@ -49,8 +48,8 @@ print(*[num**2 for num in range(n)], sep='\n')
 #Solution 3
 n = int(input())
 [print(num**2) for num in range(n)]
-
-#Problem 5
+_____________________________________________________________________________________________________________________________
+# Problem 5
 
 #Solution 1
 n = int(input())
@@ -61,8 +60,8 @@ n = int(input())
 print(*[num**2 for num in range(n)], sep = "")
 #or
 print(*range(1, int(input())+1), sep='')
-
-#Problem 6
+_____________________________________________________________________________________________________________________________
+# Problem 6
 
 #Solution 1
 def is_leap(year):
@@ -78,8 +77,8 @@ def is_leap(year):
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 year = int(input())
 print(is_leap(year))
-
-#Problem 7
+_____________________________________________________________________________________________________________________________
+# Problem 7
 
 #Solution 1
 x,y,z,n = (int(input()) for i in range(4))
