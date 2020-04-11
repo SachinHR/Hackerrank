766718b2095882d59585028e5dda1f888d026f9d
Sachin H R <sachinhr.ran@gmail.com>
2020-04-11 21:46:06 +0530

    Add eight set code

--- Python/Sets.py
+++ Python/Sets.py
@@ -30,3 +30,59 @@ ________________________________________________________________________________
 #Solution 1
 a,b = [set(input().split()) for _ in range(4)][1::2]
 print(len(a.union(b)))
+_______________________________________________________________________________________________________________________________________
+# Problem 6 : Set .intersection() Operation
+
+#Solution 1
+a,b = [set(input().split()) for _ in range(4)][1::2]
+print(len(a.intersection(b)))
+_______________________________________________________________________________________________________________________________________
+# Problem 7 : Set .difference() Operation
+
+#Solution 1
+a,b = [set(input().split()) for _ in range(4)][1::2]
+print(len(a.difference(b)))
+_______________________________________________________________________________________________________________________________________
+# Problem 8 : Set .symmetric_difference() Operation
+
+#Solution 1
+a,b = [set(input().split()) for _ in range(4)][1::2]
+print(len(a.symmetric_difference(b)))
+_______________________________________________________________________________________________________________________________________
+# Problem 9 : Set Mutations
+
+#Solution 1
+n,a = [set(input().split()) for _ in range(2)]
+for _ in range(int(input())):
+    cmd, *args = input().split()
+    getattr(a,cmd)(set(input().split()))
+print(sum(map(int,a)))
+_______________________________________________________________________________________________________________________________________
+# Problem 10 : The Captain's Room
+
+#Solution 1
+k,arr = int(input()),list(map(int,input().split()))
+print(((sum(set(arr))*k)- (sum(arr)))//(k-1))
+_______________________________________________________________________________________________________________________________________
+# Problem 11 : Check Subset
+
+#Solution 1
+for _ in range(int(input())):
+    x,a,y,b = input(), set(input().split()), input(), set(input().split())
+    print(a.issubset(b))
+_______________________________________________________________________________________________________________________________________
+# Problem 12 : Check Strict Superset
+
+#Solution 1
+a = set(input().split())
+is_strict_superset = []
+for i in range(int(input())):
+     is_strict_superset.append(a.issuperset(set(input().split(' '))))
+print(all(is_strict_superset))
+_______________________________________________________________________________________________________________________________________
+# Problem 13 : No Idea!
+
+#Solution 1
+n,m = input().split()
+s, a, b = input().split(), set(input().split()), set(input().split())
+print(sum([(i in a) - (i in b) for i in s]))
